9fd2e0261fca87ea4a5bd5ff7e603f9b16ab59d3
Emilia Tulan <emilia.tulan@gmail.com>
2021-01-20 21:10:35 +0100

    Revise the code

--- index.js
+++ index.js
@@ -1,52 +1,21 @@
-var randomColor = require('randomcolor'); // import the script from randomColor
-var color = randomColor(); // a hex code for an attractive color
-
-const chalk = require('chalk'); // import the script from chalk
-const log = console.log;
+const randomColor = require('randomcolor'); // a hex code for a hex color
+const chalk = require('chalk'); // a chalk code 
 
 //console.log(process.argv[2]);
-const myColor = process.argv[2];
+//console.log(process.argv[3]);
 
-// Returns a hex code for a light blue
-const inputColor = randomColor ({
-    luminosity: 'light',
-    hue: myColor
+const myColor = randomColor({
+    luminosity: process.argv[3],
+    hue: process.argv[2]
  });
- 
- //console.log(myColor);
-//console.log(inputColor);
 
- let stringHash = '';
- let i = 0;
- do {
-     stringHash = stringHash + '#';
-    i++;
-} while (i < 31);
-
- //console.log(stringHash);
-
- let stringHashsht = '';
- let a = 0;
- do {
-     if (a < 5 || a >= 26) {
-         stringHashsht = stringHashsht + '#';
-    }else {
-        stringHashsht = stringHashsht + '';
-    }
-    a++;
-    } while (a < 31);
-  //console.log(stringHashsht);
-
- for (const myColor = process.argv[2]) {
-     chalk.hex(myColor);
- }
-
-console.log(stringHash);
-console.log(stringHash);
-console.log('####       '+ '       ' + '        #####');
-console.log('####       '+ inputColor + '        #####');
-console.log('####       '+ '       ' + '        #####');
-console.log(stringHash);
-console.log(stringHash);
-console.log(stringHash);
-
+//prints a hex color in a colored square
+console.log(chalk.hex(myColor)('##################################'));
+console.log(chalk.hex(myColor)('##################################'));
+console.log(chalk.hex(myColor)('##################################'));
+console.log(chalk.hex(myColor)('##########            ############'));
+console.log(chalk.hex(myColor)('##########   '+myColor+'  ############'));
+console.log(chalk.hex(myColor)('##########            ############'));
+console.log(chalk.hex(myColor)('##################################'));
+console.log(chalk.hex(myColor)('##################################'));
+console.log(chalk.hex(myColor)('##################################'));
