378669142f280563984010a2b12db8ab6611099d
Emilia Tulan <emilia.tulan@gmail.com>
2021-01-21 14:17:13 +0100

    Add the js file

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const randomColor = require('randomcolor'); // a hex code for a hex color
-const chalk = require('chalk'); // a chalk code 
+randomColor = require('randomcolor'); // a hex code for a hex color
+const chalk = require('chalk'); // a chalk code
 
 //console.log(process.argv[2]);
 //console.log(process.argv[3]);
 
 const myColor = randomColor({
-    luminosity: process.argv[3],
-    hue: process.argv[2]
- });
+  luminosity: process.argv[3],
+  hue: process.argv[2],
+});
 
 //prints a hex color in a colored square
 console.log(chalk.hex(myColor)('##################################'));
 console.log(chalk.hex(myColor)('##################################'));
 console.log(chalk.hex(myColor)('##################################'));
 console.log(chalk.hex(myColor)('##########            ############'));
-console.log(chalk.hex(myColor)('##########   '+myColor+'  ############'));
+console.log(chalk.hex(myColor)('##########   ' + myColor + '  ############'));
 console.log(chalk.hex(myColor)('##########            ############'));
 console.log(chalk.hex(myColor)('##################################'));
 console.log(chalk.hex(myColor)('##################################'));
