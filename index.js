546c6a25e3baf13d48e3ff9c6afa65088227ce6b
Emilia Tulan <emilia.tulan@gmail.com>
2021-01-22 13:37:35 +0100

    Make the master happy

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-randomColor = require('randomcolor'); // a hex code for a hex color
+const randomColor = require('randomColor'); // a hex code for a hex color
 const chalk = require('chalk'); // a chalk code
 
-//console.log(process.argv[2]);
-//console.log(process.argv[3]);
+// console.log(process.argv[2]);
+// console.log(process.argv[3]);
 
 const myColor = randomColor({
   luminosity: process.argv[3],
   hue: process.argv[2],
 });
 
-//prints a hex color in a colored square
+// prints a hex color in a colored square
 console.log(chalk.hex(myColor)('##################################'));
 console.log(chalk.hex(myColor)('##################################'));
 console.log(chalk.hex(myColor)('##################################'));
