2bbf2796ef6e3bbcc1ee89786875e925fe22eba4
Emilia Tulan <emilia.tulan@gmail.com>
2021-01-27 14:22:55 +0100

    Modify code according to comments

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const randomColor = require('randomcolor'); // a hex code for a hex color
-const chalk = require('chalk'); // a chalk code
-
-// console.log(process.argv[2]);
-// console.log(process.argv[3]);
+const randomColor = require('randomcolor'); // a color hex  library
+const chalk = require('chalk'); // a chalk library
+const longString = '##################################';
+const shortString = '##########';
 
 const myColor = randomColor({
   luminosity: process.argv[3],
@@ -10,12 +9,16 @@ const myColor = randomColor({
 });
 
 // prints a hex color in a colored square
-console.log(chalk.hex(myColor)('##################################'));
-console.log(chalk.hex(myColor)('##################################'));
-console.log(chalk.hex(myColor)('##################################'));
-console.log(chalk.hex(myColor)('##########            ############'));
-console.log(chalk.hex(myColor)('##########   ' + myColor + '  ############'));
-console.log(chalk.hex(myColor)('##########            ############'));
-console.log(chalk.hex(myColor)('##################################'));
-console.log(chalk.hex(myColor)('##################################'));
-console.log(chalk.hex(myColor)('##################################'));
+
+console.log(
+  chalk.hex(myColor)(`
+##################################
+##################################
+##################################
+##########            ############
+##########   ${myColor}  ############
+##########            ############
+##################################
+##################################
+##################################`),
+);
